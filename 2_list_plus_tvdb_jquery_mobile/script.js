7010466f831c8d7e33976d0b39a5b61dee74e385
sideshows <sydow.andre@gmail.com>
2016-01-29 18:30:39 +0100

    Functional save id
    And big clean

--- 2_list_plus_tvdb_jquery_mobile/script.js
+++ 2_list_plus_tvdb_jquery_mobile/script.js
@@ -76,3 +76,59 @@ var ajax = {
 
 
 
+////////////LIST SAVED MOVIES LOCAL STORAGE
+
+function appendTaskToList(val) {
+    $('#list').append("<li>" + movieInfo.id + "  <a href='#' class='done-btn'>Done</a> <a href='#' class='cancel-btn'>Cancel Task</a></li>");
+ 
+}
+
+
+if (localStorage['tasks']) {
+    var tasks = JSON.parse(localStorage['tasks']);
+}else {
+    var tasks = [];
+}
+
+for(var i=0;i<tasks.length;i++) {
+    appendTaskToList(tasks[i]);
+}
+
+var addTask = function(){
+    // get value from #name input
+    var val = $('#name').val();
+    
+    // add the task to the array
+    tasks.push(val);
+    
+    // save to local storage
+    localStorage["tasks"] = JSON.stringify(tasks);
+    
+    // append the name to the list
+    appendTaskToList(val);
+    
+    // reset the input field and focus it.
+    $('#name').val("").focus();
+}
+
+$('#add-btn').click(addTask);
+$('#name').keyup(function(e){
+    if (e.keyCode === 13) {
+        addTask();
+    }
+});
+
+
+// approach 1
+/*$('.done-btn').click(function(){
+    $(this).parent('li').addClass('done');
+});*/
+
+
+
+   window.localStorage.clear();
+
+////////////LIST SAVED MOVIES LOCAL STORAGE -----END-----
+
+
+
